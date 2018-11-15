73c2b972010174286b1f41c3e1992487d1445d5b
kowalcj0 <kowalczykjanusz@gmail.com>
2018-11-15 14:56:15 +0000

    add support for chrome

--- behave/environment.py
+++ behave/environment.py
@@ -1,21 +1,47 @@
-from selenium.webdriver.remote import webdriver
+# -*- coding: utf-8 -*-
+import os
 
-WEBDRIVER_URL = 'http://selenium-hub:4444/wd/hub'
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
-DEFAULT_BROWSER = 'firefox'
-
+HUB_URL = os.environ.get("HUB_URL", "http://selenium-hub:4444/wd/hub")
+BROWSER = os.environ.get("BROWSER", "firefox")
 DESIRED_CAPABILITIES = {
-    'platform': 'ANY',
-    'browserName': DEFAULT_BROWSER,
-    'version': '',
-    'javascriptEnabled': True
+    "platform": "ANY",
+    "browserName": BROWSER,
+    "version": "",
+    "javascriptEnabled": True,
 }
 
 
 def before_all(context):
-    context.browser = webdriver.WebDriver(
-        WEBDRIVER_URL,
-        desired_capabilities=DESIRED_CAPABILITIES
+
+    browser = DESIRED_CAPABILITIES["browserName"].lower()
+
+    if browser == "chrome":
+        options = ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--window-size=1600x2200")
+        options.add_argument("--start-maximized")
+        options.add_argument("--whitelisted-ips=")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--no-sandbox")
+    elif browser == "firefox":
+        options = FirefoxOptions()
+        # see comment in docker-compose.yml re: bug in geckodriver
+        # options.add_argument("--headless")
+        # options.add_argument("--window-size 1600,2200")
+        options.add_argument("--width=1600")
+        options.add_argument("--height=2200")
+        options.add_argument("--safe-mode")
+    else:
+        raise Exception(f"{browser} is not supported try: Chrome or Firefox")
+
+    context.browser = webdriver.Remote(
+        command_executor=HUB_URL,
+        desired_capabilities=DESIRED_CAPABILITIES,
+        options=options,
     )
 
 
