eefc2eae687f16d0384bc8afbf2dfa32fae40fd9
kowalcj0 <kowalczykjanusz@gmail.com>
2018-11-15 15:04:43 +0000

    run tests in both firefox & chrome

--- run_test.py
+++ run_test.py
@@ -1,11 +1,12 @@
+# -*- coding: utf-8 -*-
 import sys
 
 from tasks import delegate_test
 
 
-def main(scenario):
-    delegate_test.delay(scenario)
+def main(browser: str, scenario: str):
+    delegate_test.delay(browser, scenario)
 
 
-if __name__ == '__main__':
-    main(sys.argv[1])
+if __name__ == "__main__":
+    main(browser=sys.argv[1], scenario=sys.argv[2])
--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-docker-compose exec worker behave -d -f mini behave/features/ --no-summary | while read -r line ; do
-  docker-compose exec -d worker python3 run_test.py "$line"
+docker-compose exec worker sh -c "behave --dry-run -f mini --no-summary --tags=-wip behave/features/django_admin/" | while read -r line ; do
+  docker-compose exec -d worker python3 run_test.py "chrome" "${line}"
+  docker-compose exec -d worker python3 run_test.py "firefox" "${line}"
   echo $line
 done
