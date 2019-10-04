f449086bd3009ae78b6a04220420b88d4face522
kowalcj0 <kowalczykjanusz@gmail.com>
2019-10-04 20:35:48 +0100

    moved steps & features to root dir
    and add attachments to allure report

--- features/tutorial.feature
+++ features/tutorial.feature
@@ -1,5 +1,6 @@
 Feature: showing off behave
 
+  @google
   Scenario: run a simple test
      Given we have behave installed
       When we implement a test
---
+++ steps/tutorial.py
@@ -0,0 +1,21 @@
+import allure
+from behave import given, when, then
+from behave.runner import Context
+
+
+@given("we have behave installed")
+def step_impl(context):
+    pass
+
+
+@when("we implement a test")
+def step_impl(context):
+    assert True is not False
+
+
+@then("selenium works!")
+def step_impl(context: Context):
+    context.browser.get("https://google.com")
+    screenshot = context.browser.get_screenshot_as_png()
+    allure.attach("selenium works!", name="attachment", attachment_type=allure.attachment_type.TEXT)
+    allure.attach(screenshot, name="screenshot.png", attachment_type=allure.attachment_type.PNG)
