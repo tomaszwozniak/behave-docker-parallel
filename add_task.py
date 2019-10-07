5f6a36f8124a65723f2f769f53f04ae66c520f49
kowalcj0 <kowalczykjanusz@gmail.com>
2019-10-07 09:46:22 +0100

    remove redundat function

--- add_task.py
+++ add_task.py
@@ -4,9 +4,5 @@ import sys
 from tasks import delegate_test
 
 
-def main(browser: str, scenario: str):
-    delegate_test.delay(browser, scenario)
-
-
 if __name__ == "__main__":
-    main(browser=sys.argv[1], scenario=sys.argv[2])
+    delegate_test.delay(browser=sys.argv[1], scenario=sys.argv[2])
